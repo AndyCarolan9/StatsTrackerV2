ef0fb174b13662a596e8a253c64dd7291990a98f
Andrew Carolan <andycarolan9@gmail.com>
2026-01-06 19:41:14 +0000

    Created team selection pickers which update player lists

--- Data/Constants.cs
+++ Data/Constants.cs
@@ -4,7 +4,12 @@ namespace StatsTrackerV2.Data
     {
         public const string DatabaseFilename = "AppSQLite.db3";
 
+        public const string TeamsJSONFilename = "Teams.json";
+
         public static string DatabasePath =>
             $"Data Source={Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename)}";
+
+        public static string TeamsJSONPath =>
+            $"{Path.Combine(FileSystem.AppDataDirectory, "Teams", TeamsJSONFilename)}";
     }
 }
--- PageModels/CreateMatchPageModel.cs
+++ PageModels/CreateMatchPageModel.cs
@@ -1,5 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using StatsTrackerV2.Models;
+using System.Collections.ObjectModel;
 
 namespace StatsTrackerV2.PageModels
 {
@@ -7,9 +9,120 @@ namespace StatsTrackerV2.PageModels
     {
         private Match _match;
 
+        private List<Team> _teams;
+
+        private string _selectedHomeTeam;
+        public string SelectedHomeTeam
+        {
+            get => _selectedHomeTeam;
+            set
+            {
+                if (_selectedHomeTeam != value)
+                {
+                    _selectedHomeTeam = value;
+                    OnSelectedHomeTeamChanged();
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        private string _selectedAwayTeam;
+        public string SelectedAwayTeam
+        {
+            get => _selectedAwayTeam;
+            set
+            {
+                if (_selectedAwayTeam != value)
+                {
+                    _selectedAwayTeam = value;
+                    OnSelectedAwayTeamChanged();
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        [ObservableProperty]
+        private ObservableCollection<string> _teamNames = [];
+
+        [ObservableProperty]
+        private ObservableCollection<string> _homeTeamPlayers = [];
+
+        [ObservableProperty]
+        private ObservableCollection<string> _awayTeamPlayers = [];
+
         public CreateMatchPageModel(Match match)
         {
-            _match = match; 
+            _match = match;
+            _teams = new List<Team>();
+            _selectedHomeTeam = string.Empty;
+            _selectedAwayTeam = string.Empty;
+        }
+
+        [RelayCommand]
+        private async Task Appearing()
+        {
+            try
+            {
+                Team[]? teams = JSONHelper.LoadFromJsonFile<Team[]>(Constants.TeamsJSONPath);
+                if (teams == null)
+                {
+                    return;
+                }
+
+                foreach(Team team in teams)
+                {
+                    TeamNames.Add(team.TeamName);
+                }
+
+                _teams.AddRange(teams);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private void OnSelectedHomeTeamChanged()
+        {
+            HomeTeamPlayers.Clear();
+
+            if (SelectedHomeTeam == string.Empty)
+            { 
+                // Clear starting players array
+            }
+
+            Team? selectedTeam = _teams.Find(team => team.TeamName.Equals(SelectedHomeTeam));
+            if (selectedTeam == null)
+            {
+                return;
+            }
+
+            foreach(string player in selectedTeam.TeamSheet)
+            {
+                HomeTeamPlayers.Add(player);
+            }
+        }
+
+        private void OnSelectedAwayTeamChanged()
+        {
+            AwayTeamPlayers.Clear();
+
+            if (SelectedAwayTeam == string.Empty)
+            {
+                // Clear starting players array
+            }
+
+            Team? selectedTeam = _teams.Find(team => team.TeamName.Equals(SelectedAwayTeam));
+            if (selectedTeam == null)
+            {
+                return;
+            }
+
+            foreach (string player in selectedTeam.TeamSheet)
+            {
+                AwayTeamPlayers.Add(player);
+            }
         }
     }
 }
