621663706b82527cd31ff6440b2671a8d4ed58c1
Andrew Carolan <andycarolan9@gmail.com>
2026-04-04 16:25:14 +0100

    Added Platform helpers class

---
+++ Utilities/PlatformHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace StatsTrackerV2.Utilities
+{
+    public static class PlatformHelper
+    {
+        public static bool IsDesktop =>
+            DeviceInfo.Current.Platform == DevicePlatform.WinUI ||
+            DeviceInfo.Current.Platform == DevicePlatform.MacCatalyst;
+
+        public static bool IsMobile =>
+            DeviceInfo.Current.Platform == DevicePlatform.Android ||
+            DeviceInfo.Current.Platform == DevicePlatform.iOS;
+    }
+}
