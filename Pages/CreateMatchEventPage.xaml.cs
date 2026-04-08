ab96d1dc5a4cf716d891e449312e4702dff42086
Andrew Carolan <andycarolan9@gmail.com>
2026-04-08 17:13:10 +0100

    Change pitch display in create match event view

--- Pages/CreateMatchEventPage.xaml.cs
+++ Pages/CreateMatchEventPage.xaml.cs
@@ -9,31 +9,4 @@ public partial class CreateMatchEventPage : ContentPage
 		InitializeComponent();
 		BindingContext = model;
 	}
-
-	private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
-    {
-		BoxView? boxView = sender as BoxView;
-		if (boxView == null)
-		{
-			return;
-		}
-
-		CreateMatchEventPageModel? vm = BindingContext as CreateMatchEventPageModel;
-        if (vm == null)
-        {
-			return;
-        }
-
-		Point? point = e.GetPosition(boxView);
-		if(point == null)
-		{
-			return; 
-		}
-
-		float x = (float)point.Value.X / (float)boxView.Width;
-		float y = (float)point.Value.Y / (float)boxView.Height;
-
-		vm.Location = new PointF(x, y);
-		DrawingView.Invalidate();
-    }
 }
