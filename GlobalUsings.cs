584da291cf1ebc67a05799e2cc77f313ce7afd1c
Andrew Carolan <andycarolan9@gmail.com>
2026-04-23 21:02:00 +0100

    Removed example code

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -3,5 +3,4 @@ global using StatsTrackerV2.Data;
 global using StatsTrackerV2.Enums;
 global using StatsTrackerV2.PageModels;
 global using StatsTrackerV2.Pages;
-global using StatsTrackerV2.Services;
 global using StatsTrackerV2.Utilities;
--- MauiProgram.cs
+++ MauiProgram.cs
@@ -24,14 +24,6 @@ namespace StatsTrackerV2
     				{
     					handler.PlatformView.SingleSelectionFollowsFocus = false;
     				});
-
-    				Microsoft.Maui.Handlers.ContentViewHandler.Mapper.AppendToMapping(nameof(Pages.Controls.CategoryChart), (handler, view) =>
-    				{
-    					if (view is Pages.Controls.CategoryChart && handler.PlatformView is Microsoft.Maui.Platform.ContentPanel contentPanel)
-    					{
-    						contentPanel.IsTabStop = true;
-    					}
-    				});
 #endif
                 })
                 .ConfigureFonts(fonts =>
@@ -47,23 +39,12 @@ namespace StatsTrackerV2
     		builder.Services.AddLogging(configure => configure.AddDebug());
 #endif
 
-            builder.Services.AddSingleton<ProjectRepository>();
-            builder.Services.AddSingleton<TaskRepository>();
-            builder.Services.AddSingleton<CategoryRepository>();
-            builder.Services.AddSingleton<TagRepository>();
-            builder.Services.AddSingleton<SeedDataService>();
-            builder.Services.AddSingleton<ModalErrorHandler>();
-            builder.Services.AddSingleton<MainPageModel>();
-            builder.Services.AddSingleton<ProjectListPageModel>();
-            builder.Services.AddSingleton<ManageMetaPageModel>();
             builder.Services.AddSingleton<Match>();
             builder.Services.AddSingleton<MatchPageModel>();
             builder.Services.AddSingleton<KickoutStatsPageModel>();
             builder.Services.AddSingleton<TurnoverStatsPageModel>();
             builder.Services.AddSingleton<ShotStatsPageModel>();
 
-            builder.Services.AddTransientWithShellRoute<ProjectDetailPage, ProjectDetailPageModel>("project");
-            builder.Services.AddTransientWithShellRoute<TaskDetailPage, TaskDetailPageModel>("task");
             builder.Services.AddTransientWithShellRoute<OpenMatchPage, OpenMatchPageModel>("openMatch");
             builder.Services.AddTransientWithShellRoute<CreateMatchPage, CreateMatchPageModel>("createMatch");
             builder.Services.AddTransientWithShellRoute<TeamListPage, TeamListPageModel>("teamList");
